0a4faf56d1738e556c3d235ee355b3c7a653336b
unknown <aydindallitakte@gmail.com>
2020-08-21 22:16:16 +0200

    Day05 ders sonu

---
+++ src/com/techproed/Day05_CssSelector.java
@@ -0,0 +1,37 @@
+package com.techproed;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Day05_CssSelector {
+    public static void main(String[] args) {
+
+        // id, name, classname, tagname, linkText, partialLinkText, xpath, cssSelector
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Documents\\Selenium dependencies\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://a.testaddressbook.com/sign_in");
+
+        // Eger class name icinde eger bosluk varsa cssSelector kullanmalisiniz
+        // cssSelector kullanirken , class value kullaniyorsak .value
+        WebElement emailKutusu = driver.findElement(By.cssSelector(".form-control"));
+        // cssSelector kullanirken , id value kullaniyorsak    #isim
+        //WebElement emailKutusu = driver.findElement(By.cssSelector("#session_email"));
+        emailKutusu.sendKeys("testtechproed@gmail.com");
+
+        // tagName[attribute name='value']
+        WebElement sifreKutusu = driver.findElement(By.cssSelector("input[placeholder='Password']"));
+        sifreKutusu.sendKeys("Test1234!");
+
+        sifreKutusu.submit();
+
+
+
+    }
+}
--- src/com/techproed/Day05_Xpath02.java
+++ src/com/techproed/Day05_Xpath02.java
@@ -5,27 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.SQLOutput;
 import java.util.concurrent.TimeUnit;
 
-public class Day04_GoogleTest {
+public class Day05_Xpath02 {
     public static void main(String[] args) {
-
         System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Documents\\Selenium dependencies\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("http://google.com");
+        driver.get("http://a.testaddressbook.com/");
 
-        WebElement aramaKutusu = driver.findElement(By.name("q"));
-        aramaKutusu.sendKeys("city bike");
-        aramaKutusu.submit();
-
-        WebElement sonucSayisi = driver.findElement(By.id("result-stats"));
-        System.out.println(sonucSayisi.getText());
-
-        WebElement alisverisLinki = driver.findElement(By.partialLinkText("beelding"));
-        alisverisLinki.click();
-
+        // xpath kullanımında, attribute kullanmaya gerek duymazsanız,sadece tagName yazılarakta webelement bulunabilir.
+        WebElement welcomeYazisi = driver.findElement(By.xpath("//h1"));
+        System.out.println(welcomeYazisi.getText());
 
 
     }
